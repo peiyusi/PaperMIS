2083e9793ba3c30e9a277c155aa9a712eeb25d6d
youname <youeamil@email.com>
2018-05-24 14:58:49 +0800

    add database

---
+++ app/Http/Controllers/admin/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Model\Users;
+
+class UserController extends Controller
+{
+	public function index() {
+//		$user = Users::all();
+//		return $user;
+		echo 'test1';
+	}
+}
