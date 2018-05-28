aa59946b9605c82b4b120658f37a36d46208c08a
youname <youeamil@email.com>
2018-05-28 21:09:45 +0800

    modified studentInformation.blade.php

--- resources/views/student/studentInformation.blade.php
+++ resources/views/student/studentInformation.blade.php
@@ -40,7 +40,7 @@
                                 <div class="am-form-group">
                                     <label for="user-phone" class="am-u-sm-3 am-form-label">电话 / Telephone</label>
                                     <div class="am-u-sm-9">
-                                        <input type="tel" id="user-phone" readonly="" autocompletse="off" disable value="">
+                                        <input type="tel" id="user-phone" placeholder="电话 / Telephone" value="">
                                     </div>
                                 </div>
 
